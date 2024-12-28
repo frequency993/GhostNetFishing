8e9b0871c189e5dca8737d374fc58d65f571fa3b
Dennis <93009809+frequency993@users.noreply.github.com>
2024-12-28 11:30:55 +0100

    Beta
    Fixed #3: Persistenz ist fertig.
    
    Persistence is ready

--- GhostNetFishing/src/main/java/GeisternetzDAO.java
+++ GhostNetFishing/src/main/java/GeisternetzDAO.java
@@ -13,6 +13,29 @@ public class GeisternetzDAO {
 	public void speichern(Geisternetz geisternetz) {
     	EntityManager em = emf.createEntityManager();
         EntityTransaction t = em.getTransaction();
+        
+		// Prüfung, ob die MeldendePerson im Geisternetz vorhanden ist und die ID null ist.
+		// Aktuell sollte dies beim Aufruf von speichern() immer der Fall sein
+		MeldendePerson meldendePerson = geisternetz.getMeldendePerson();
+		if (meldendePerson != null && meldendePerson.getId() == 0) {
+		
+            // Prüfung, ob eine MeldendePerson mit den Attributen bereits in der Datenbank vorhanden ist
+			// abfrage ist komplexer, da Telefonnummer NULL sein kann
+            String abfrage = "SELECT p FROM Person p WHERE p.vorname = :vorname AND p.nachname = :nachname " +
+            				 "AND ((:telefonnummer IS NULL AND p.telefonnummer IS NULL) OR p.telefonnummer = :telefonnummer)";
+            TypedQuery<MeldendePerson> query = em.createQuery(abfrage, MeldendePerson.class);
+            query.setParameter("vorname", meldendePerson.getVorname());
+            query.setParameter("nachname", meldendePerson.getNachname());
+            query.setParameter("telefonnummer", meldendePerson.getTelefonnummer());
+            List<MeldendePerson> ergebnis = query.getResultList();
+            
+            if (!ergebnis.isEmpty()) {
+                // Wenn die MeldendePerson bereits existiert, wird diese Person verwendet
+                meldendePerson = ergebnis.get(0);
+                geisternetz.setMeldendePerson(meldendePerson);
+            }
+		}
+        
         t.begin();
         em.persist(geisternetz);
         t.commit();
@@ -22,10 +45,9 @@ public class GeisternetzDAO {
 	public void updateBergendePerson(Geisternetz geisternetz, BergendePerson bergendePerson) {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction t = em.getTransaction();
-		geisternetz.setBergendePerson(bergendePerson);
 		t.begin();
-		//BergendePerson managedBergendePerson = em.find(BergendePerson.class, bergendePerson.getId());
-		//geisternetz.setBergendePerson(managedBergendePerson);
+		BergendePerson managedBergendePerson = em.find(BergendePerson.class, bergendePerson.getId());
+		geisternetz.setBergendePerson(managedBergendePerson);
 		em.merge(geisternetz);
 		t.commit();
 		em.close();
@@ -34,6 +56,27 @@ public class GeisternetzDAO {
 	public void update(Geisternetz geisternetz) {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction t = em.getTransaction();
+		
+		// Prüfung, ob die VerschollenMeldendePerson im Geisternetz vorhanden ist und die ID null ist.
+		// Aktuell sollte dies beim Aufruf von update() immer der Fall sein
+		Person meldendePerson = geisternetz.getVerschollenMeldendePerson();
+		if (meldendePerson != null && meldendePerson.getId() == 0) {
+		
+            // Prüfung, ob eine VerschollenMeldendePerson mit den Attributen bereits in der Datenbank vorhanden ist
+            String abfrage = "SELECT p FROM Person p WHERE p.vorname = :vorname AND p.nachname = :nachname AND p.telefonnummer = :telefonnummer";
+            TypedQuery<Person> query = em.createQuery(abfrage, Person.class);
+            query.setParameter("vorname", meldendePerson.getVorname());
+            query.setParameter("nachname", meldendePerson.getNachname());
+            query.setParameter("telefonnummer", meldendePerson.getTelefonnummer());
+            List<Person> ergebnis = query.getResultList();
+            
+            if (!ergebnis.isEmpty()) {
+                // Wenn die VerschollenMeldendePerson bereits existiert, wird diese Person verwendet
+                meldendePerson = ergebnis.get(0);
+                geisternetz.setVerschollenMeldendePerson(meldendePerson);
+            }
+		}
+		
 		t.begin();
 		em.merge(geisternetz);
 		t.commit();
